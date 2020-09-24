abc2c917cf9a2fe3f2270bad3e3be540214451a2
HillPhelmuth <46843652+HillPhelmuth@users.noreply.github.com>
2020-09-24 01:25:48 -0500

    Move comptile function

---
+++ Api/CompileCode.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using System.Threading.Tasks;
+using BlazorApp.Api.Services;
+using BlazorApp.Shared.CodeModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace BlazorApp.Api.Functions.Compile
+{
+    public class CompileCode
+    {
+        private readonly CompilerService _compilerService;
+        public CompileCode(CompilerService compilerService)
+        {
+            _compilerService = compilerService;
+        }
+
+        [FunctionName("CompileCode")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "code")] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP/code trigger function processed a request.");
+
+            var executableReferences = CompileResources.PortableExecutableReferences;
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var codeInput = JsonConvert.DeserializeObject<CodeInputModel>(requestBody);
+            var testCode = codeInput.Solution;
+            var result = await _compilerService.SubmitCode(testCode, executableReferences);
+            
+            return new OkObjectResult(result);
+        }
+
+
+    }
+}
---
+++ Api/CompileResources.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace BlazorApp.Api.Functions.Compile
+{
+    public class CompileResources
+    {
+        
+
+        
+
+        public static List<PortableExecutableReference> PortableExecutableReferences
+        {
+            get
+            {
+                var appAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+                var narrowedAssemblies = appAssemblies.Where(x =>
+                    !x.IsDynamic && !string.IsNullOrWhiteSpace(x.Location) &&
+                    (x.FullName.Contains("System")));
+
+                return narrowedAssemblies.Select(assembly => MetadataReference.CreateFromFile(assembly.Location)).ToList();
+            }
+        }
+    }
+}
