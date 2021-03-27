326dee01cf3c698e915fc88b3728b046c030495c
HillPhelmuth <46843652+HillPhelmuth@users.noreply.github.com>
2021-03-27 13:00:15 -0500

    fix suggestions code to include all available libraries.

--- Api/Functions/Compile/CompleteCode.cs
+++ Api/Functions/Compile/CompleteCode.cs
@@ -53,25 +53,29 @@ namespace BlazorApp.Api.Functions.Compile
             }
 
         }
-        private static IEnumerable<PortableExecutableReference> _ref;
-        public static async Task<dynamic> GetCodeCompletion(SourceInfo sourceInfo, ILogger log)
+        private static IEnumerable<PortableExecutableReference> _staticRefs;
+        public static async Task<dynamic> GetCodeCompletion(SourceInfo sourceInfo, ILogger log = null)
         {
-            var thisAssembly = typeof(CompleteCode).Assembly;
-            var loadContext = AssemblyLoadContext.GetLoadContext(thisAssembly);
             var refs = CompileResources.PortableExecutableCompletionReferences;
 
-            var usings = new List<string>();
-            var allusingsInCode = sourceInfo.SourceCode.Split(new string[] { "using " }, StringSplitOptions.None);
-
-            foreach (var item in allusingsInCode)
+            var usings = new List<string>()
             {
-                if (!string.IsNullOrWhiteSpace(item))
-                {
-                    usings.Add(item.Split(';')[0]);
-                }
-            }
-
-            List<Assembly> assemblies = loadContext.Assemblies.Where(a => !a.IsDynamic && File.Exists(a.Location)).ToList();
+                "System",
+                "System.IO",
+                "System.Collections.Generic",
+                "System.Collections",
+                "System.Console",
+                "System.Diagnostics",
+                "System.Dynamic",
+                "System.Linq",
+                "System.Linq.Expressions",
+                "System.Net.Http",
+                "System.Text",
+                "System.Net",
+                "System.Threading.Tasks",
+                "System.Numerics"
+            };
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.IsDynamic && File.Exists(a.Location)).ToList();
 
             var partTypes = MefHostServices.DefaultAssemblies.Concat(assemblies)
                     .Distinct()?
@@ -85,90 +89,65 @@ namespace BlazorApp.Api.Functions.Compile
 
             var workspace = new AdhocWorkspace(host);
 
-            var scriptCode = sourceInfo.SourceCode;
+            string scriptCode = sourceInfo.SourceCode;
             var _ = typeof(Microsoft.CodeAnalysis.CSharp.Formatting.CSharpFormattingOptions);
             var compilationOptions = new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, usings: usings);
-            if (_ref == null || !_ref.Any())
+            if (refs == null || refs.Count == 0) return null;
+
+            var scriptProjectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "Script", "Script", LanguageNames.CSharp, isSubmission: true)
+                .WithMetadataReferences(refs)
+                .WithCompilationOptions(compilationOptions);
+
+            var scriptProject = workspace.AddProject(scriptProjectInfo);
+            var scriptDocumentInfo = DocumentInfo.Create(
+                DocumentId.CreateNewId(scriptProject.Id), "Script",
+                sourceCodeKind: SourceCodeKind.Script,
+                loader: TextLoader.From(TextAndVersion.Create(SourceText.From(scriptCode), VersionStamp.Create())));
+            var scriptDocument = workspace.AddDocument(scriptDocumentInfo);
+
+            // cursor position is at the end
+            int position = sourceInfo.LineNumberOffsetFromTemplate;
+            var completionService = CompletionService.GetService(scriptDocument);
+            var results = await completionService.GetCompletionsAsync(scriptDocument, position);
+            if (results == null && sourceInfo.LineNumberOffsetFromTemplate < sourceInfo.SourceCode.Length)
             {
-                var _refs = assemblies
-                 .Select(asm => MetadataReference.CreateFromFile(asm.Location))
-                 .ToArray();
-                _ref = new PortableExecutableReference[0];
-
-
-                _ref = _refs.Concatenate(_refs);
+                sourceInfo.LineNumberOffsetFromTemplate++;
+                await GetCodeCompletion(sourceInfo, log);
             }
-            if (_ref?.Count() > 0)
+
+            if (sourceInfo.SourceCode[sourceInfo.LineNumberOffsetFromTemplate - 1].ToString() == "(")
             {
-                var _ref = refs.Concatenate(new[]
-                {
-                     MetadataReference.CreateFromFile(typeof(object).Assembly.Location)
-                });
-                var scriptProjectInfo = ProjectInfo.Create(ProjectId.CreateNewId(), VersionStamp.Create(), "Script", "Script", LanguageNames.CSharp, isSubmission: true)
-                    .WithMetadataReferences(refs)
-                   .WithCompilationOptions(compilationOptions);
-
-                var scriptProject = workspace.AddProject(scriptProjectInfo);
-                var scriptDocumentInfo = DocumentInfo.Create(
-                    DocumentId.CreateNewId(scriptProject.Id), "Script",
-                    sourceCodeKind: SourceCodeKind.Script,
-                    loader: TextLoader.From(TextAndVersion.Create(SourceText.From(scriptCode), VersionStamp.Create())));
-                var scriptDocument = workspace.AddDocument(scriptDocumentInfo);
-
-                // cursor position is at the end
-                var position = sourceInfo.LineNumberOffsetFromTemplate;
-                var completionService = CompletionService.GetService(scriptDocument);
-                var results = await completionService.GetCompletionsAsync(scriptDocument, position);
-                if (results == null && sourceInfo.LineNumberOffsetFromTemplate < sourceInfo.SourceCode.Length)
-                {
-                    sourceInfo.LineNumberOffsetFromTemplate++;
-                    await GetCodeCompletion(sourceInfo, log);
-                }
-
-                if (sourceInfo.SourceCode[sourceInfo.LineNumberOffsetFromTemplate - 1].ToString() == "(")
-                {
-                    sourceInfo.LineNumberOffsetFromTemplate--;
-                    results = completionService.GetCompletionsAsync(scriptDocument, sourceInfo.LineNumberOffsetFromTemplate).Result;
-                }
-                //Method parameters
-                List<string> overloads = GetMethodParams(scriptCode, position, log);
-                if (overloads?.Count > 0)
-                {
-                    ImmutableArray<CompletionItem>.Builder builder = ImmutableArray.CreateBuilder<CompletionItem>();
-                    foreach (var item in overloads)
-                    {
-                        string DisplayText = item;
-                        string insertText = item.Split('(')[1].Split(')')[0];
-                        CompletionItem ci = CompletionItem.Create(insertText, insertText, insertText);
-                        builder.Add(ci);
-                    }
-                    if (builder.Count > 0)
-                    {
-                        ImmutableArray<CompletionItem> itemlist = builder.ToImmutable();
-                        return CompletionList.Create(new TextSpan(), itemlist);
-                    }
-                }
-                return results;
+                sourceInfo.LineNumberOffsetFromTemplate--;
+                results = completionService.GetCompletionsAsync(scriptDocument, sourceInfo.LineNumberOffsetFromTemplate).Result;
             }
-            return null;
+
+            //Method parameters
+            var overloads = GetMethodParams(scriptCode, position, log);
+            if (!(overloads?.Count > 0)) return results;
+            var builder = ImmutableArray.CreateBuilder<CompletionItem>();
+            foreach (var ci in overloads.Select(item => new { item, DisplayText = item })
+                .Select(@t => @t.item.Split('(')[1].Split(')')[0])
+                .Select(insertText => CompletionItem.Create(insertText, insertText, insertText)))
+            {
+                builder.Add(ci);
+            }
+
+            if (builder.Count <= 0) return results;
+            var itemlist = builder.ToImmutable();
+            return CompletionList.Create(new TextSpan(), itemlist);
+
         }
         public static List<string> GetMethodParams(string scriptCode, int position, ILogger log = null)
         {
             //position = position - 2;
-            List<string> overloads = new List<string>();
+            var overloads = new List<string>();
             var meta = AppContext.GetData("TRUSTED_PLATFORM_ASSEMBLIES");
-            string[] assembliesNames = meta.ToString().Split(';', StringSplitOptions.None);
+            Console.WriteLine($"Trusted Platform Assemblies: {meta}");
+            string[] assembliesNames = meta.ToString().Split(';');
             var sourceLanguage = new CSharpLanguage();
-            //SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(scriptCode);
-            SyntaxTree syntaxTree = sourceLanguage.ParseText(scriptCode, SourceCodeKind.Script);
-            var root = (CompilationUnitSyntax)syntaxTree.GetRoot();
-            var thisAssembly = typeof(CompleteCode).Assembly;
-            var loadContext = AssemblyLoadContext.GetLoadContext(thisAssembly);
-
-
-            var Mscorlib = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
+            var syntaxTree = sourceLanguage.ParseText(scriptCode, SourceCodeKind.Script);
             var compilation = CSharpCompilation.Create("MyCompilation",
-                syntaxTrees: new[] { syntaxTree }, references: CompileResources.PortableExecutableCompletionReferences);
+                new[] { syntaxTree }, CompileResources.PortableExecutableCompletionReferences);
 
             var model = compilation.GetSemanticModel(syntaxTree);
 
@@ -180,42 +159,35 @@ namespace BlazorApp.Api.Functions.Compile
                 if (theNode == null) break; // There isn't an InvocationExpression in this branch of the tree
             }
 
-            if (theNode == null)
+            if (theNode != null)
             {
-                overloads = null;
+                var symbolInfo = model.GetSymbolInfo(theNode);
+
+                if (symbolInfo.CandidateSymbols.Length > 0)
+                {
+                    overloads.AddRange(symbolInfo.CandidateSymbols
+                        .Select(param => new { parameters = param, i = param.ToMinimalDisplayParts(model, position) })
+                        .Where(@t => @t.parameters.Kind == SymbolKind.Method)
+                        .Select(@t => @t.parameters.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat)));
+                }
             }
             else
             {
-                var symbolInfo = model.GetSymbolInfo(theNode);
-                var symbol = symbolInfo.Symbol;
-                var containingType = symbol?.ContainingType;
-
-                if (symbolInfo.CandidateSymbols != default && symbolInfo.CandidateSymbols.Length > 0)
-                {
-                    foreach (var parameters in symbolInfo.CandidateSymbols)
-                    {
-                        var i = parameters.ToMinimalDisplayParts(model, position);
-                        if (parameters.Kind == SymbolKind.Method)
-                        {
-                            var mp = parameters.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
-                            overloads.Add(mp);
-                        }
-                    }
-                }
+                overloads = null;
             }
-            return overloads;
 
+            return overloads;
         }
         public class CSharpLanguage : ILanguageService
         {
-            private readonly LanguageVersion MaxLanguageVersion = Enum
+            private readonly LanguageVersion _maxLanguageVersion = Enum
                 .GetValues(typeof(LanguageVersion))
                 .Cast<LanguageVersion>()
                 .Max();
 
             public SyntaxTree ParseText(string sourceCode, SourceCodeKind kind)
             {
-                var options = new CSharpParseOptions(kind: kind, languageVersion: MaxLanguageVersion);
+                var options = new CSharpParseOptions(kind: kind, languageVersion: _maxLanguageVersion);
 
                 // Return a syntax tree of our source code
                 return CSharpSyntaxTree.ParseText(sourceCode, options);
